0482f9c54233e8096e1b41f115446a6ae49eb8a9
Ryo Yanagida <ryo@vagrant-ndnsim>
2024-07-02 11:08:17 +0000

    debugging queue behaviours

--- examples/ndn-prio-queue-small-0521_1147.cpp
+++ examples/ndn-prio-queue-small-0521_1147.cpp
@@ -109,27 +109,12 @@ main(int argc, char* argv[])
   strftime(buf, sizeof buf, "%Y-%m-%d-%H%M", gmtime(&now));
   string prefix = "";
   string date = buf;
-  string exp_name = "ndn-prioqueue-small";
-  uint32_t std_prefix_rate = 300; //default 300 pps
-  uint32_t prio_prefix_rate = 300; //default 300 pps
-  uint32_t queueSize=10;
-  string out_dir = "./";
-  uint64_t run_num = 1; //defaut run number
-
-  CommandLine cmd (__FILE__);
-  cmd.AddValue("stdPRate", "Standard packet rate (pps) per app instance", std_prefix_rate);
-  cmd.AddValue("prioPRate", "Prioritised packet rate (pps) per app instance", prio_prefix_rate);
-  cmd.AddValue("outDir", "Output directory",out_dir);
-  cmd.AddValue("expName", "Experiment name override", exp_name);
-  cmd.AddValue("runNum", "NS3 Run Number",run_num);
-  cmd.AddValue("date","Date string override",date);
-  cmd.Parse(argc,argv);
-  ns3::RngSeedManager::SetRun(run_num); 
+  string exp_name = "ndn-prioqueue-small-";
   prefix.append(date);
-  prefix.append("-"+exp_name);
-  prefix.append("-"+std::to_string(run_num));
-  prefix.append("-"+std::to_string(std_prefix_rate)+"_std_pps");
-  prefix.append("-"+std::to_string(prio_prefix_rate)+"_prio_pps");
+  prefix.append(exp_name);
+
+
+  uint32_t queueSize=10;
   // setting default parameters for PointToPoint links and channels
   Config::SetDefault("ns3::PointToPointNetDevice::DataRate", StringValue("2.0Mbps"));
   Config::SetDefault("ns3::PointToPointChannel::Delay", StringValue("20ms"));
@@ -137,14 +122,14 @@ main(int argc, char* argv[])
   // Devices queue configuration
   //Config::SetDefault ("ns3::DropTailQueue<Packet>::MaxSize",
                        //QueueSizeValue (QueueSize (QueueSizeUnit::PACKETS, queueSize)));
-  std::cout << prefix << std::endl;
+
   // Read optional command-line parameters (e.g., enable visualizer with ./waf --run=<> --visualize
- // CommandLine cmd;
- // cmd.Parse(argc, argv);
+  CommandLine cmd;
+  cmd.Parse(argc, argv);
 
   // Creating nodes
   NodeContainer nodes;
-  nodes.Create(6);
+  nodes.Create(5);
   std::cout << "Nodes created" << std::endl;
 
 
@@ -169,7 +154,7 @@ main(int argc, char* argv[])
 
   
   PointToPointHelper bottleneckLink;
-  bottleneckLink.SetDeviceAttribute ("DataRate", StringValue ("10Mbps"));
+  bottleneckLink.SetDeviceAttribute ("DataRate", StringValue ("100Mbps"));
   bottleneckLink.SetChannelAttribute("Delay", StringValue("20ms"));
   bottleneckLink.SetQueue("ns3::DropTailQueue","MaxSize", StringValue ("100p"));
 
@@ -184,60 +169,46 @@ main(int argc, char* argv[])
  //devices.Add(p2p.Install(nodes.Get(2), nodes.Get(3)));
  //p2p.SetQueue("ns3::DropTailQueue","MaxSize", StringValue("50p"));
 
-  string PrioQDMap = "1 1 1 1 0 1 1 1 1 1 1 1 1 1 1 1";
-  string PrioQDMapFlip = "0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0";
   //consumerLink
   TrafficControlHelper tchConsumerLink;
-  uint16_t tchConsumerLinkHandle = tchConsumerLink.SetRootQueueDisc("ns3::PrioQueueDisc","Priomap", StringValue(PrioQDMap));
-  //tchConsumerLink.AddInternalQueues(tchConsumerLinkHandle,2, "ns3::DropTailQueue", "MaxSize", StringValue("100p"));
+  uint16_t tchConsumerLinkHandle = tchConsumerLink.SetRootQueueDisc("ns3::PrioQueueDisc","Priomap", StringValue("1 1 1 1 0 1 1 1 1 1 1 1 1 1 1 1"));
   TrafficControlHelper::ClassIdList cidConsumerLink = tchConsumerLink.AddQueueDiscClasses(tchConsumerLinkHandle, 2, "ns3::QueueDiscClass");
   uint16_t consumerQdhandle0 = tchConsumerLink.AddChildQueueDisc(tchConsumerLinkHandle,cidConsumerLink[0], "ns3::FifoQueueDisc");
-  uint16_t consumerQdhandle1 = tchConsumerLink.AddChildQueueDisc(tchConsumerLinkHandle,cidConsumerLink[1], "ns3::FifoQueueDisc");
+  uint16_t consumerQdhandle1 = tchConsumerLink.AddChildQueueDisc(tchConsumerLinkHandle,cidConsumerLink[1], "ns3::RedQueueDisc");
   tchConsumerLink.AddPacketFilter(tchConsumerLinkHandle,"ns3::ndn::NdnPacketFilter");
   
   tchConsumerLink.AddInternalQueues(consumerQdhandle0, 1, "ns3::DropTailQueue","MaxSize",StringValue("100p"));
   tchConsumerLink.AddInternalQueues(consumerQdhandle1, 1, "ns3::DropTailQueue","MaxSize",StringValue("100p"));
-  
-  //tchConsumerLink.AddInternalQueues(tchConsumerLinkHandle, 2, "ns3::DropTailQueue","MaxSize",StringValue("100p")); 
  
 //producerLink
   TrafficControlHelper tchProducerLink;
-  uint16_t tchProducerLinkHandle = tchProducerLink.SetRootQueueDisc("ns3::PrioQueueDisc","Priomap", StringValue(PrioQDMap));
-  //tchProducerLink.AddInternalQueues(tchProducerLinkHandle,2, "ns3::DropTailQueue", "MaxSize", StringValue("100p"));
+  uint16_t tchProducerLinkHandle = tchProducerLink.SetRootQueueDisc("ns3::PrioQueueDisc","Priomap", StringValue("1 1 1 1 0 1 1 1 1 1 1 1 1 1 1 1"));
   TrafficControlHelper::ClassIdList cidProducerLink = tchProducerLink.AddQueueDiscClasses(tchProducerLinkHandle, 2, "ns3::QueueDiscClass");
   uint16_t producerQdhandle0 = tchProducerLink.AddChildQueueDisc(tchProducerLinkHandle,cidProducerLink[0], "ns3::FifoQueueDisc");
-  uint16_t producerQdhandle1 = tchProducerLink.AddChildQueueDisc(tchProducerLinkHandle,cidProducerLink[1], "ns3::FifoQueueDisc");
+  uint16_t producerQdhandle1 = tchProducerLink.AddChildQueueDisc(tchProducerLinkHandle,cidProducerLink[1], "ns3::RedQueueDisc");
   tchProducerLink.AddPacketFilter(tchProducerLinkHandle,"ns3::ndn::NdnPacketFilter");
   
   tchProducerLink.AddInternalQueues(producerQdhandle0, 1, "ns3::DropTailQueue","MaxSize",StringValue("100p"));
   tchProducerLink.AddInternalQueues(producerQdhandle1, 1, "ns3::DropTailQueue","MaxSize",StringValue("100p"));
-
- // tchProducerLink.AddInternalQueues(tchProducerLinkHandle, 2, "ns3::DropTailQueue","MaxSize",StringValue("100p")); 
-
-
+ 
 //bottleneck
   TrafficControlHelper tchBottleneckLink;
-  uint16_t tchBottleneckLinkHandle = tchBottleneckLink.SetRootQueueDisc("ns3::PrioQueueDisc","Priomap", StringValue(PrioQDMap));
-  //tchBottleneckLink.AddInternalQueues(tchBottleneckLinkHandle,2, "ns3::DropTailQueue", "MaxSize", StringValue("100p"));
+  uint16_t tchBottleneckLinkHandle = tchBottleneckLink.SetRootQueueDisc("ns3::PrioQueueDisc","Priomap", StringValue("1 1 1 1 0 1 1 1 1 1 1 1 1 1 1 1"));
   TrafficControlHelper::ClassIdList cidBottleneckLink = tchBottleneckLink.AddQueueDiscClasses(tchBottleneckLinkHandle, 2, "ns3::QueueDiscClass");
   uint16_t bottleneckQdhandle0 = tchBottleneckLink.AddChildQueueDisc(tchBottleneckLinkHandle,cidBottleneckLink[0], "ns3::FifoQueueDisc");
-  uint16_t bottleneckQdhandle1 = tchBottleneckLink.AddChildQueueDisc(tchBottleneckLinkHandle,cidBottleneckLink[1], "ns3::FifoQueueDisc");
+  uint16_t bottleneckQdhandle1 = tchBottleneckLink.AddChildQueueDisc(tchBottleneckLinkHandle,cidBottleneckLink[1], "ns3::RedQueueDisc");
   tchBottleneckLink.AddPacketFilter(tchBottleneckLinkHandle,"ns3::ndn::NdnPacketFilter");
   
   tchBottleneckLink.AddInternalQueues(bottleneckQdhandle0, 1, "ns3::DropTailQueue","MaxSize",StringValue("100p"));
   tchBottleneckLink.AddInternalQueues(bottleneckQdhandle1, 1, "ns3::DropTailQueue","MaxSize",StringValue("100p"));
-
-//  tchBottleneckLink.AddInternalQueues(tchBottleneckLinkHandle, 2, "ns3::DropTailQueue","MaxSize",StringValue("100p")); 
 
 
   NetDeviceContainer devicesConsumerLink = consumerLink.Install (nodes.Get(0),nodes.Get(1));
   devicesConsumerLink.Add(consumerLink.Install (nodes.Get(4),nodes.Get(1)));
 
   NetDeviceContainer devicesProducerLink = producerLink.Install (nodes.Get(3),nodes.Get(2));
-  devicesProducerLink.Add( producerLink.Install(nodes.Get(5),nodes.Get(2)));
 
   NetDeviceContainer devicesBottleneckLink = bottleneckLink.Install (nodes.Get(1),nodes.Get(2));
-  std::cout << "links created" << std::endl;
   // Install NDN stack on all nodes
   ndn::StackHelper ndnHelper;
   ndnHelper.SetDefaultRoutes(true);
@@ -248,26 +219,35 @@ main(int argc, char* argv[])
 
   tchProducerLink.Install(devicesProducerLink);
 
-  QueueDiscContainer qdiscs =  tchBottleneckLink.Install(devicesBottleneckLink);
+  tchBottleneckLink.Install(devicesBottleneckLink);
 
- // Ptr<TrafficControlLayer> tc = devicesConsumerLink.Get(0)->GetNode ()->GetObject<TrafficControlLayer> ();
- // tc->ScanDevices();
- // Ptr<TrafficControlLayer> tc4 = devicesConsumerLink.Get(2)->GetNode ()->GetObject<TrafficControlLayer> ();
- // tc4->ScanDevices();
- // Ptr<TrafficControlLayer> tc1 = devicesProducerLink.Get(0)->GetNode ()->GetObject<TrafficControlLayer> ();
- // tc1->ScanDevices();
- // Ptr<TrafficControlLayer> tc2 = devicesBottleneckLink.Get(0)->GetNode ()->GetObject<TrafficControlLayer> ();
- // tc2->ScanDevices();
- // Ptr<TrafficControlLayer> tc3 = devicesBottleneckLink.Get(1)->GetNode ()->GetObject<TrafficControlLayer> ();
- // tc3->ScanDevices();
-  for(int i = 0; i < 6; i++){
-    std::cout << "Setting up node:" << i << std::endl;
-    //CreateAndAggregateObjectFromTypeId (nodes.Get(i), "ns3::TrafficControlLayer");
-    Ptr<TrafficControlLayer> tc = nodes.Get(i)->GetObject<TrafficControlLayer>();
-    tc->ScanDevices();
-  }
+  Ptr<TrafficControlLayer> tc = devicesConsumerLink.Get(0)->GetNode ()->GetObject<TrafficControlLayer> ();
+  tc->ScanDevices();
+  Ptr<TrafficControlLayer> tc4 = devicesConsumerLink.Get(2)->GetNode ()->GetObject<TrafficControlLayer> ();
+  tc4->ScanDevices();
+  Ptr<TrafficControlLayer> tc1 = devicesProducerLink.Get(0)->GetNode ()->GetObject<TrafficControlLayer> ();
+  tc1->ScanDevices();
+  Ptr<TrafficControlLayer> tc2 = devicesBottleneckLink.Get(0)->GetNode ()->GetObject<TrafficControlLayer> ();
+  tc2->ScanDevices();
+  Ptr<TrafficControlLayer> tc3 = devicesBottleneckLink.Get(1)->GetNode ()->GetObject<TrafficControlLayer> ();
+  tc3->ScanDevices();
+
 
  
+  // traffic-control-layer
+ // TrafficControlLayer tcl0 = TrafficControlLayer();
+ // TrafficControlLayer tcl1 = TrafficControlLayer();
+ // TrafficControlLayer tcl2 = TrafficControlLayer();
+ // tcl0.SetNode(nodes.Get(0));
+ //
+ // tcl1.SetNode(nodes.Get(1));
+ // tcl2.SetNode(nodes.Get(2));
+ // std::cout << "TCL: nodes set" <<std::endl; 
+ // CreateAndAggregateObjectFromTypeId (nodes.Get(0), "ns3::TrafficControlLayer");
+ // CreateAndAggregateObjectFromTypeId (nodes.Get(1), "ns3::TrafficControlLayer");
+ // CreateAndAggregateObjectFromTypeId (nodes.Get(2), "ns3::TrafficControlLayer");
+
+
 
 // set queue
   //setup traffic control 
@@ -287,17 +267,13 @@ main(int argc, char* argv[])
  // tch.AddInternalQueues(qdhandle0, 2, "ns3::DropTailQueue","MaxSize",StringValue("100p"));
  // tch.AddInternalQueues(qdhandle1, 2, "ns3::DropTailQueue","MaxSize",StringValue("100p"));
 
-  std::cout<<"setting up queue telem"<<std::endl;
-  std::cout << qdiscs.GetN() << std::endl;
-  Ptr<QueueDisc> q = qdiscs.Get (0);
-  q->TraceConnectWithoutContext ("PacketsInQueue", MakeCallback (&TcPacketsInQueueTrace));
-  Config::ConnectWithoutContext ("/NodeList/1/$ns3::TrafficControlLayer/RootQueueDiscList/0/SojournTime",
-                                 MakeCallback (&SojournTimeTrace));
-  Ptr<QueueDisc> q2 = qdiscs.Get (1);
-  q2->TraceConnectWithoutContext ("PacketsInQueue", MakeCallback (&TcPacketsInQueueTrace));
-  Config::ConnectWithoutContext ("/NodeList/2/$ns3::TrafficControlLayer/RootQueueDiscList/0/SojournTime",
-                                 MakeCallback (&SojournTimeTrace));
 
+  //QueueDiscContainer qdiscs = tchBottleneckLink.Install (devices);
+
+  //Ptr<QueueDisc> q = qdiscs.Get (3);
+  //q->TraceConnectWithoutContext ("PacketsInQueue", MakeCallback (&TcPacketsInQueueTrace));
+  //Config::ConnectWithoutContext ("/NodeList/1/$ns3::TrafficControlLayer/RootQueueDiscList/0/SojournTime",
+  //                               MakeCallback (&SojournTimeTrace));
 
   //Ptr<NetDevice> nd = devices.Get(3);
   //Ptr<PointToPointNetDevice> ptpnd = DynamicCast<PointToPointNetDevice>(nd);
@@ -317,96 +293,68 @@ main(int argc, char* argv[])
  // Ptr<TrafficControlLayer> tc8 = devices.Get(8)->GetNode ()->GetObject<TrafficControlLayer> (); 
  // tc8 -> ScanDevices();
 
-  string streaming_service_a = "/streaming_service_A";
-  string social_network_a = "/social_network_A";
+
   
 
 // Install NDN stack on all nodes
   //ndn::StackHelper ndnHelper;
   //ndnHelper.SetDefaultRoutes(true);
   //ndnHelper.InstallAll();
-  std::cout<<"setting up strategy"<<endl; 
+ 
     // Choosing forwarding strategy
-  ndn::StrategyChoiceHelper::InstallAll(social_network_a,"/localhost/nfd/strategy/multicast");
-  ndn::StrategyChoiceHelper::InstallAll(streaming_service_a,"/localhost/nfd/strategy/multicast");
+  ndn::StrategyChoiceHelper::InstallAll("/prefix", "/localhost/nfd/strategy/multipath");
+  ndn::StrategyChoiceHelper::InstallAll("/prio","/localhost/nfd/strategy/multipath");
   // Installing applications
 
   // Consumer
-  //ndn::AppHelper consumerHelper("ns3::ndn::ConsumerCbr");
+  ndn::AppHelper consumerHelper("ns3::ndn::ConsumerCbr");
   // Consumer will request /prefix/0, /prefix/1, ...
-  //consumerHelper.SetPrefix("/prefix");
-  //consumerHelper.SetAttribute("Frequency", StringValue("300")); //  interests a second
-   
-  ndn::AppHelper consumerHelperN4("ns3::ndn::ConsumerCbr");
-  consumerHelperN4.SetPrefix(social_network_a+"/static");
-  consumerHelperN4.SetAttribute("Frequency", StringValue(std::to_string(std_prefix_rate))); //  interests a second
-
-
-  //ndn::AppHelper prioConsumerHelper("ns3::ndn::ConsumerCbr");
-  //prioConsumerHelper.SetPrefix("/prio");
-  //prioConsumerHelper.SetAttribute("Frequency", StringValue("3000")); 
-
-  ndn::AppHelper prioConsumerHelperN0("ns3::ndn::ConsumerCbr");
-  prioConsumerHelperN0.SetPrefix(streaming_service_a+"/live/formula1");
-  prioConsumerHelperN0.SetAttribute("Frequency", StringValue(std::to_string(prio_prefix_rate))); 
-
+  consumerHelper.SetPrefix("/prefix");
+  consumerHelper.SetAttribute("Frequency", StringValue("160")); //  interests a second
   
-  auto prioappsN0 = prioConsumerHelperN0.Install(nodes.Get(0));
-  prioappsN0.Stop(Seconds(10.0));
-
-  auto appsN4 = consumerHelperN4.Install(nodes.Get(4));
-  appsN4.Stop(Seconds(10.0));
-
+  auto apps = consumerHelper.Install(nodes.Get(0));                        // first node
+  apps.Stop(Seconds(10.0)); // stop the consumer app at 10 seconds mark
+  
+  ndn::AppHelper prioConsumerHelper("ns3::ndn::ConsumerCbr");
+  prioConsumerHelper.SetPrefix("/prio");
+  prioConsumerHelper.SetAttribute("Frequency", StringValue("140")); 
+  
+  auto prioapps = prioConsumerHelper.Install(nodes.Get(0));
+  prioapps.Stop(Seconds(10.0));
+ 
+  auto prioapps2 = prioConsumerHelper.Install(nodes.Get(4));
+  prioapps2.Stop(Seconds(10.0));
+//
 
   // Producer
   ndn::AppHelper producerHelper("ns3::ndn::Producer");
   // Producer will reply to all requests starting with /prefix
-  producerHelper.SetPrefix(social_network_a);
+  producerHelper.SetPrefix("/prefix");
   producerHelper.SetAttribute("PayloadSize", StringValue("1024"));
   producerHelper.Install(nodes.Get(3)); // last node
  
 // Producer
   ndn::AppHelper prioProducerHelper("ns3::ndn::Producer");
   // Producer will reply to all requests starting with /prefix
-  prioProducerHelper.SetPrefix(streaming_service_a);
+  prioProducerHelper.SetPrefix("/prio");
   prioProducerHelper.SetAttribute("PayloadSize", StringValue("1024"));
-  prioProducerHelper.Install(nodes.Get(5)); // last node
+  prioProducerHelper.Install(nodes.Get(3)); // last node
   
 
   // Application-layer Latency tracer
-  ndn::AppDelayTracer::InstallAll(out_dir+prefix+"-adt-all.txt");
-  ndn::AppDelayTracer::Install(nodes.Get(0),out_dir+prefix+"-adt-n0.txt"); 
-  ndn::AppDelayTracer::Install(nodes.Get(4),out_dir+prefix+"-adt-n4.txt");
+  ndn::AppDelayTracer::InstallAll(prefix+"app-delays-trace.txt");
+  ndn::AppDelayTracer::Install(nodes.Get(0), prefix+"app-delays-trace-n0.txt"); 
+ // ndn::AppDelayTracer::Install(nodes.Get(3),"app-delays-trace-n3.txt");
+ // ndn::AppDelayTracer::Install(nodes.Get(4),"app-delays-trace-n4.txt");
 
 
-  PcapWriter traceN0(out_dir+prefix+"-ndn_sim_n0.pcap");
-  Config::ConnectWithoutContext("/NodeList/0/DeviceList/*/$ns3::PointToPointNetDevice/MacTx",
-		  MakeCallback(&PcapWriter::TracePacket, &traceN0));
-  Config::ConnectWithoutContext("/NodeList/0/DeviceList/*/$ns3::PointToPointNetDevice/MacRx",
-		  MakeCallback(&PcapWriter::TracePacket, &traceN0));
+  PcapWriter trace("ndn-simple-trace-link.pcap");
+  Config::ConnectWithoutContext("/NodeList/2/DeviceList/*/$ns3::PointToPointNetDevice/MacTx",
+		  MakeCallback(&PcapWriter::TracePacket, &trace));
+  Config::ConnectWithoutContext("/NodeList/2/DeviceList/*/$ns3::PointToPointNetDevice/MacRx",
+		  MakeCallback(&PcapWriter::TracePacket, &trace));
 
-  PcapWriter traceN4(out_dir+prefix+"-ndn_sim_n4.pcap");
-  Config::ConnectWithoutContext("/NodeList/4/DeviceList/*/$ns3::PointToPointNetDevice/MacTx",
-		  MakeCallback(&PcapWriter::TracePacket, &traceN4));
-  Config::ConnectWithoutContext("/NodeList/4/DeviceList/*/$ns3::PointToPointNetDevice/MacRx",
-		  MakeCallback(&PcapWriter::TracePacket, &traceN4));
-
-  PcapWriter traceN3(out_dir+prefix+"-ndn_sim_n3.pcap");
-  Config::ConnectWithoutContext("/NodeList/3/DeviceList/*/$ns3::PointToPointNetDevice/MacTx",
-		  MakeCallback(&PcapWriter::TracePacket, &traceN3));
-  Config::ConnectWithoutContext("/NodeList/3/DeviceList/*/$ns3::PointToPointNetDevice/MacRx",
-		  MakeCallback(&PcapWriter::TracePacket, &traceN3));
-
-
-  PcapWriter traceN5(out_dir+prefix+"-ndn_sim_n5.pcap");
-  Config::ConnectWithoutContext("/NodeList/5/DeviceList/*/$ns3::PointToPointNetDevice/MacTx",
-		  MakeCallback(&PcapWriter::TracePacket, &traceN5));
-  Config::ConnectWithoutContext("/NodeList/5/DeviceList/*/$ns3::PointToPointNetDevice/MacRx",
-		  MakeCallback(&PcapWriter::TracePacket, &traceN5));
-
-
-
-  Simulator::Stop(Seconds(10.0));
+  Simulator::Stop(Seconds(20.0));
   std::cout << "Start the simulation" << std::endl;
   Simulator::Run();
   Simulator::Destroy();
